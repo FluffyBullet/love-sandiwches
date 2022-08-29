137de8991f4977feeabf98207a1c31c35c4f3759
FluffyBullet <davidreynolds34@hotmail.co.uk>
2022-08-29 13:10:52 +0000

    add sales data to sales worksheet

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -21,7 +18,7 @@ def get_sales_data():
 
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, seperated by commas.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here:")
@@ -30,9 +27,9 @@ def get_sales_data():
         validate_data(sales_data)
 
         if validate_data(sales_data):
-            print("data is valid!")
+            print("Data is valid!")
             break
-    return sales_data(values)
+    return sales_data
 
 def validate_data(values):
     """
@@ -52,4 +49,16 @@ def validate_data(values):
 
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
